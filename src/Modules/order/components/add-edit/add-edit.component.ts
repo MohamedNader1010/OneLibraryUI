0edae4f8d24a6caac460757f247b9c6acea21d74
waleed-negm <wm.negm@gmail.com>
2023-01-04 09:23:14 +0200

    add update functionality to orders form

--- src/Modules/order/components/add-edit/add-edit.component.ts
+++ src/Modules/order/components/add-edit/add-edit.component.ts
@@ -24,6 +24,32 @@ export class AddEditComponent implements OnInit {
 	constructor(private router: Router, private route: ActivatedRoute, private _order: OrderService, private _material: MaterialService, private _type: ServicesTypeService, private fb: FormBuilder) {
 		this.orderForm = this.createFormItem("init");
 	}
+	ngOnInit(): void {
+		// this.getAllDetails();
+		// this.getAllTransactions();
+		// this.route.queryParams.subscribe((params) => (this.id = params["id"]));
+		// if (this.id) this.getSingle(this.id);
+		this.fillFormWithData(this.formDatasource);
+	}
+	formDatasource: any = {
+		totalPrice: 3,
+		rest: 0,
+		paid: 3,
+		status: false,
+		clientId: 1,
+		details: [
+			{
+				price: 1,
+				serviceId: 1,
+				noteId: 1,
+			},
+			{
+				price: 2,
+				serviceId: 2,
+				noteId: 2,
+			},
+		],
+	};
 	createFormItem(type: string): FormGroup {
 		let formItem: FormGroup = this.fb.group({});
 		switch (type) {
@@ -47,40 +73,23 @@ export class AddEditComponent implements OnInit {
 		}
 		return formItem;
 	}
-	ngOnInit(): void {
-		// this.getAllDetails();
-		// this.getAllTransactions();
-		this.route.queryParams.subscribe((params) => (this.id = params["id"]));
-		if (this.id) this.getSingle(this.id);
+	fillFormWithData(datasource: any) {
+		datasource.details.forEach(() => this.handleNewDetail());
+		this.orderForm.patchValue(datasource);
 	}
 	get OrderDetails(): FormArray {
 		return this.orderForm.get("details") as FormArray;
 	}
-	getSingle = (id: number) => this._order.getOne(id).subscribe((data) => {});
+	getSingle = (id: number) => this._order.getOne(id).subscribe((data) => (this.formDatasource = data));
 	// getAllDetails = () => this._details.getAll().subscribe({next: (data) => (this.DetailsDataSource = data)});
 	// getAllTransactions = () => this._transactions.getAll().subscribe({next: (data) => (this.TransactionsDataSource = data)});
-	back = () => this.router.navigate(["services"]);
-	handleNewDetail = () => {
-		this.OrderDetails.push(this.createFormItem("detail"));
-	};
-	handleDeleteDetail = (index: number) => {
-		this.OrderDetails.removeAt(index);
-	};
+	back = () => this.router.navigate(["orders"]);
+	handleNewDetail = () => this.OrderDetails.push(this.createFormItem("detail"));
+	handleDeleteDetail = (index: number) => this.OrderDetails.removeAt(index);
 	handleSubmit() {
 		if (this.orderForm.valid) {
-			if (this.id) {
-				this._order.update(this.id, this.orderForm.value).subscribe({
-					next: () => {
-						this.router.navigate(["services"]);
-					},
-				});
-			} else {
-				this._order.add(this.orderForm.value).subscribe({
-					next: () => {
-						this.router.navigate(["services"]);
-					},
-				});
-			}
+			if (this.id) this._order.update(this.id, this.orderForm.value).subscribe(() => this.back());
+			else this._order.add(this.orderForm.value).subscribe(() => this.back());
 		}
 	}
 }
--- src/Modules/order/components/all/all.component.ts
+++ src/Modules/order/components/all/all.component.ts
@@ -7,8 +7,6 @@ import {MatTable, MatTableDataSource} from "@angular/material/table";
 import {Router} from "@angular/router";
 import {DialogComponent} from "src/Modules/shared/components/dialog/dialog.component";
 import {Order} from "../../interfaces/Iorder";
-import {OrderDetail} from "../../interfaces/IorderDetail";
-import {OrderTransaction} from "../../interfaces/IorderTransaction";
 import {OrderService} from "../../services/orders.service";
 
 @Component({
@@ -18,8 +16,6 @@ import {OrderService} from "../../services/orders.service";
 })
 export class AllComponent implements OnInit {
 	@ViewChild(MatTable) servicesTable!: MatTable<any>;
-	DetailsDataSource: OrderDetail[] = [];
-	TransactionsDataSource: OrderTransaction[] = [];
 	constructor(private _router: Router, private _liveAnnouncer: LiveAnnouncer, private _order: OrderService, public dialog: MatDialog) {}
 	columns = [
 		{
@@ -68,8 +64,6 @@ export class AllComponent implements OnInit {
 	@ViewChild(MatSort) sort!: MatSort;
 	dataSource: MatTableDataSource<Order> = new MatTableDataSource<Order>([]);
 	ngOnInit(): void {
-		// this.getAllDetails();
-		// this.getAllTransactions();
 		this.getAll();
 	}
 	getAll() {
@@ -79,11 +73,8 @@ export class AllComponent implements OnInit {
 				this.dataSource.paginator = this.paginator;
 				this.dataSource.sort = this.sort;
 			},
-			// error: (e) => console.log(e),
 		});
 	}
-	// getAllDetails = () => this._details.getAll().subscribe({next: (data) => (this.DetailsDataSource = data)});
-	// getAllTransactions = () => this._transactions.getAll().subscribe({next: (data) => (this.TransactionsDataSource = data)});
 	dcols = this.columns.map((c) => c.columnDef);
 	displayedColumns = [...this.dcols, "actions"];
 	announceSortChange = (sortState: Sort) => (sortState.direction ? this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`) : this._liveAnnouncer.announce("Sorting cleared"));
