9d27d5d3ded4d5f706d90f58070458314a2af2ca
waleed-negm <wm.negm@gmail.com>
2023-01-18 14:48:48 +0200

    add note route

--- src/Modules/shared/routing/shared-routing.module.ts
+++ src/Modules/shared/routing/shared-routing.module.ts
@@ -1,25 +1,25 @@
-import {NgModule} from "@angular/core";
-import {RouterModule, Routes} from "@angular/router";
-import {NotfoundComponent} from "../components/notfound/notfound.component";
-import {SharedComponent} from "../shared.component";
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {NotfoundComponent} from '../components/notfound/notfound.component';
+import {SharedComponent} from '../shared.component';
 
 const routes: Routes = [
 	{
-		path: "",
+		path: '',
 		component: SharedComponent,
-		title: "dashboard",
+		title: 'dashboard',
 		children: [
-			{path: "services", loadChildren: () => import("../../service/service.module").then((m) => m.ServiceModule)},
-			{path: "orders", loadChildren: () => import("../../order/order.module").then((m) => m.OrderModule)},
-			{path: "materials", loadChildren: () => import("../../material/material.module").then((m) => m.MaterialModule)},
-			{path: "serviceTypes", loadChildren: () => import("../../serviceType/serviceType.module").then((m) => m.ServiceTypeModule)},
-			{path: "clients", loadChildren: () => import("../../client/client.module").then((m) => m.ClientModule)},
-			{path: "clientTypes", loadChildren: () => import("../../clientType/clientType.module").then((m) => m.ClientTypeModule)},
-			{path: "servicePrice", loadChildren: () => import("../../service-price-per-client-type/service-price-per-client-type.module").then((m) => m.ServicePricePerClientTypeModule)},
-			
+			{path: 'services', loadChildren: () => import('../../service/service.module').then((m) => m.ServiceModule)},
+			{path: 'orders', loadChildren: () => import('../../order/order.module').then((m) => m.OrderModule)},
+			{path: 'materials', loadChildren: () => import('../../material/material.module').then((m) => m.MaterialModule)},
+			{path: 'serviceTypes', loadChildren: () => import('../../serviceType/serviceType.module').then((m) => m.ServiceTypeModule)},
+			{path: 'clients', loadChildren: () => import('../../client/client.module').then((m) => m.ClientModule)},
+			{path: 'clientTypes', loadChildren: () => import('../../clientType/clientType.module').then((m) => m.ClientTypeModule)},
+			{path: 'servicePrice', loadChildren: () => import('../../service-price-per-client-type/service-price-per-client-type.module').then((m) => m.ServicePricePerClientTypeModule)},
+			{path: 'notes', loadChildren: () => import('../../note/note.module').then((m) => m.NoteModule)},
 		],
 	},
-	{path: "**", component: NotfoundComponent, title: "404 - not found"},
+	{path: '**', component: NotfoundComponent, title: '404 - not found'},
 ];
 
 @NgModule({
