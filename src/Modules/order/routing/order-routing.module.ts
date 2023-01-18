4b5e6daf08cb23e909c127ff93ebdc45c3304b7d
waleed-negm <wm.negm@gmail.com>
2023-01-18 14:49:04 +0200

    fix order details page title

--- src/Modules/order/routing/order-routing.module.ts
+++ src/Modules/order/routing/order-routing.module.ts
@@ -1,23 +1,23 @@
-import {NgModule} from "@angular/core";
-import {RouterModule, Routes} from "@angular/router";
-import {OrderComponent} from "../order.component";
-import {AllComponent} from "../components/all/all.component";
-import {AddEditComponent} from "../components/add-edit/add-edit.component";
-import {TransactionComponent} from "../components/transaction/transaction.component";
-import {DetailsComponent} from "../components/details/details.component";
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {OrderComponent} from '../order.component';
+import {AllComponent} from '../components/all/all.component';
+import {AddEditComponent} from '../components/add-edit/add-edit.component';
+import {DetailsComponent} from '../components/details/details.component';
+import {TransactionComponent} from '../components/transaction/transaction.component';
 
 const routes: Routes = [
 	{
-		path: "",
+		path: '',
 		component: OrderComponent,
-		title: "orders",
+		title: 'orders',
 		children: [
-			{path: "all", component: AllComponent, title: "all orders"},
-			{path: "new", component: AddEditComponent, title: "add new order"},
-			{path: "edit", component: AddEditComponent, title: "edit order's data"},
-			{path: "transaction", component: TransactionComponent, title: "order transaction"},
-			{path: "details", component: DetailsComponent, title: "edit order's data"},
-			{path: "", redirectTo: "all", pathMatch: "full"},
+			{path: 'all', component: AllComponent, title: 'all orders'},
+			{path: 'new', component: AddEditComponent, title: 'add new order'},
+			{path: 'edit', component: AddEditComponent, title: "edit order's data"},
+			{path: 'transaction', component: TransactionComponent, title: 'order transaction'},
+			{path: 'details', component: DetailsComponent, title: 'order details'},
+			{path: '', redirectTo: 'all', pathMatch: 'full'},
 		],
 	},
 ];
