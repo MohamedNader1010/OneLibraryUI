41e91290689977326288d3a5b7f0bc9b308a7b36
waleed negm <68160452+waleed-negm@users.noreply.github.com>
2023-01-27 19:26:15 +0200

    handle servicePricePerClient form canDeactivate

--- src/Modules/service-price-per-client-type/guards/canDeactivateForm.guard.ts
+++ src/Modules/service-price-per-client-type/guards/canDeactivateForm.guard.ts
@@ -2,13 +2,14 @@ import {Injectable} from '@angular/core';
 import {MatDialog} from '@angular/material/dialog';
 import {CanDeactivate} from '@angular/router';
 import {DialogComponent} from '../../shared/components/dialog/dialog.component';
+import {AddEditComponent} from '../Components/add-edit/add-edit.component';
 
 @Injectable()
 // implements CanDeactivate<AddEditComponent>
 export class CanDeactivateGuard {
 	constructor(public dialog: MatDialog) {}
 	async canDeactivate(component: AddEditComponent) {
-		if ((component.Form.touched && component.Form.dirty && component.isSubmitted) || component.Form.untouched) return true;
+		if ((component.form.touched && component.form.dirty && component.isSubmitted) || component.form.untouched) return true;
 		let res: boolean = await new Promise<boolean>((resolve, reject) => {
 			this.dialog.closeAll();
 			this.dialog
