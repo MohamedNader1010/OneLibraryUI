bdb91dd9dcc3bfaa030eef87fcd2bcc5cd407f24
waleed-negm <wm.negm@gmail.com>
2023-03-30 11:05:16 +0200

    add canEdit input to table

--- src/Modules/shared/components/table/table.component.ts
+++ src/Modules/shared/components/table/table.component.ts
@@ -1,12 +1,12 @@
-import { SendDataFromTableToMatDialoge } from './../../services/sendDataFromTableToMatDialoge.service';
-import { Component, EventEmitter, Input, OnInit, OnDestroy, Output, ViewChild } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort, Sort } from '@angular/material/sort';
-import { MatTableDataSource } from '@angular/material/table';
-import { Router, ActivatedRoute } from '@angular/router';
-import { Subscription } from 'rxjs';
-import { DialogComponent } from '../dialog/dialog.component';
+import {SendDataFromTableToMatDialoge} from './../../services/sendDataFromTableToMatDialoge.service';
+import {Component, EventEmitter, Input, OnInit, OnDestroy, Output, ViewChild} from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort, Sort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+import {Router, ActivatedRoute} from '@angular/router';
+import {Subscription} from 'rxjs';
+import {DialogComponent} from '../dialog/dialog.component';
 
 @Component({
 	selector: 'app-table[dialogDisplayName][tableColumns][tableData][dialogHeader]',
@@ -14,7 +14,6 @@ import { DialogComponent } from '../dialog/dialog.component';
 	styleUrls: ['./table.component.css'],
 })
 export class TableComponent implements OnInit, OnDestroy {
-
 	subscriptions: Subscription[] = [];
 	public dataSource = new MatTableDataSource<any>([]);
 	public displayedColumns!: string[];
@@ -27,9 +26,10 @@ export class TableComponent implements OnInit, OnDestroy {
 	@Input() loading: any;
 	@Input() tableColumns: any;
 	@Input() canView: boolean = false;
+	@Input() canEdit: boolean = true;
 	@Input() hasTransaction: boolean = false;
 	@Input() isDisplayDeleteButton: boolean = true;
-	@Input() isHideAllButtons: boolean = false; 
+	@Input() isHideAllButtons: boolean = false;
 	@ViewChild(MatSort) set matSort(ms: MatSort) {
 		this.sort = ms;
 		this.setDataSourceAttributes();
@@ -45,12 +45,9 @@ export class TableComponent implements OnInit, OnDestroy {
 	setDataSourceAttributes() {
 		this.dataSource.paginator = this.paginator;
 		this.dataSource.sort = this.sort;
-
 	}
 	columns: any[] = [];
-	constructor(private _router: Router, public dialog: MatDialog, private route: ActivatedRoute, private sendRowId: SendDataFromTableToMatDialoge) {
-
-	}
+	constructor(private _router: Router, public dialog: MatDialog, private route: ActivatedRoute, private sendRowId: SendDataFromTableToMatDialoge) {}
 	ngOnInit(): void {
 		this.displayedColumns = [...this.tableColumns.map((c: any) => c.columnDef), 'actions'];
 	}
@@ -58,17 +55,17 @@ export class TableComponent implements OnInit, OnDestroy {
 		this.dataSource.filter = (event.target as HTMLInputElement).value.trim().toLowerCase();
 		if (this.dataSource.paginator) this.dataSource.paginator.firstPage();
 	};
-	HandleNew = () => this._router.navigate([`../new`], { relativeTo: this.route });
-	handleEdit = (row: any) => this._router.navigate([`../edit`], { queryParams: { id: row.id }, relativeTo: this.route });
+	HandleNew = () => this._router.navigate([`../new`], {relativeTo: this.route});
+	handleEdit = (row: any) => this._router.navigate([`../edit`], {queryParams: {id: row.id}, relativeTo: this.route});
 	handleView = (id: number) => {
 		this.OnView.emit();
 		this.sendRowId.setOrderId(id);
 	};
-	handleTransaction = (row: any) => this._router.navigate([`../transaction`], { queryParams: { id: row.id }, relativeTo: this.route });
+	handleTransaction = (row: any) => this._router.navigate([`../transaction`], {queryParams: {id: row.id}, relativeTo: this.route});
 	handleDelete = (row: any) => {
 		this.subscriptions.push(
 			this.dialog
-				.open(DialogComponent, { data: { location: this.dialogHeader, msg: `هل انت متاكد من حذف "${row[this.dialogDisplayName]}"؟` } })
+				.open(DialogComponent, {data: {location: this.dialogHeader, msg: `هل انت متاكد من حذف "${row[this.dialogDisplayName]}"؟`}})
 				.afterClosed()
 				.subscribe(() => this.OnDelete.emit(row.id))
 		);
