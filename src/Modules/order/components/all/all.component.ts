657233fcec097d336681be99b654bde8be535a11
waleed-negm <wm.negm@gmail.com>
2023-04-01 12:00:54 +0200

    order status bug fix

--- src/Modules/order/components/all/all.component.ts
+++ src/Modules/order/components/all/all.component.ts
@@ -1,12 +1,12 @@
-import { AlertServiceService } from './../../../shared/services/alert-service.service';
-import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { Subscription } from 'rxjs';
-import { Status } from '../../Enums/status';
-import { Order } from '../../interfaces/Iorder';
-import { OrderService } from '../../services/orders.service';
-import { DetailsComponent } from '../details/details.component';
-import { TranslateService } from '@ngx-translate/core';
+import {AlertServiceService} from './../../../shared/services/alert-service.service';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
+import {Subscription} from 'rxjs';
+import {Status} from '../../Enums/status';
+import {Order} from '../../interfaces/Iorder';
+import {OrderService} from '../../services/orders.service';
+import {DetailsComponent} from '../details/details.component';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
 	selector: 'app-all',
@@ -18,44 +18,44 @@ export class AllComponent implements OnInit, OnDestroy {
 	tableColumns!: any[];
 	tableData!: Order[];
 	loading!: boolean;
-	constructor(private translate:TranslateService, private _order: OrderService, public dialog: MatDialog, private alertService: AlertServiceService) { }
+	constructor(private translate: TranslateService, private _order: OrderService, public dialog: MatDialog, private alertService: AlertServiceService) {}
 	ngOnInit(): void {
 		this.tableColumns = [
 			{
 				columnDef: 'id',
-				header: this.translate.instant("order.number"),
+				header: this.translate.instant('order.number'),
 				cell: (element: Order) => `${element.id}`,
 			},
 			{
 				columnDef: 'TotalPrice',
-				header: this.translate.instant("shared.totalPrice"),
+				header: this.translate.instant('shared.totalPrice'),
 				cell: (element: Order) => `${element.totalPrice}`,
 			},
 			{
 				columnDef: 'rest',
-				header: this.translate.instant("order.rest"),
+				header: this.translate.instant('order.rest'),
 				cell: (element: Order) => `${element.rest}`,
 			},
 			{
 				columnDef: 'Paid',
-				header: this.translate.instant("order.paid"),
+				header: this.translate.instant('order.paid'),
 				cell: (element: Order) => `${element.paid}`,
 			},
 			{
 				columnDef: 'Status',
-				header: this.translate.instant("order.status"),
+				header: this.translate.instant('order.status'),
 				cell: (element: Order) => `${this.getStatusText(element.orderStatus)}`,
 			},
 			{
 				columnDef: 'Client',
-				header: this.translate.instant("shared.clientName"),
+				header: this.translate.instant('shared.clientName'),
 				cell: (element: Order) => `${element.clientName}`,
 			},
 			{
 				columnDef: 'Remarks',
-				header: this.translate.instant("shared.notice"),
-				cell: (element: Order) => element.remarks
-			}
+				header: this.translate.instant('shared.notice'),
+				cell: (element: Order) => element.remarks,
+			},
 		];
 		this.getAll();
 	}
@@ -81,7 +81,7 @@ export class AllComponent implements OnInit, OnDestroy {
 			this.dialog
 				.open(DetailsComponent)
 				.afterClosed()
-				.subscribe(() => { })
+				.subscribe(() => {})
 		);
 	}
 	handleDelete = (id: number) => this.subscriptions.push(this._order.delete(id).subscribe(() => this.getAll()));
@@ -101,7 +101,7 @@ export class AllComponent implements OnInit, OnDestroy {
 			case Status.حجز:
 				return 'حجز';
 			case Status.غير_مكتمل:
-				return 'غير مرتجع';
+				return 'غير مكتمل';
 			case Status.مرتجع:
 				return 'مرتجع';
 			default:
