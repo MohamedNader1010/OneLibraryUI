6f079ce2c91a026d31b87b947679c9000ea62d2f
Mohamed Nader <m7mdnader1010@gmail.com>
2023-04-06 17:08:09 +0200

    - modify in the structure of add and edit in tables, - finish material add-edit DialogForm - continue to implement to the whole module with the same logic - need to implement (unsavedChangesGuard, @HostListiner events to the new components)

--- src/Modules/shared/components/dialog/dialog.component.ts
+++ src/Modules/shared/components/dialog/dialog.component.ts
@@ -1,5 +1,6 @@
-import {Component, Inject} from "@angular/core";
-import {MAT_DIALOG_DATA} from "@angular/material/dialog";
+import { Component, EventEmitter, Inject, Output } from "@angular/core";
+import { MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { DialogServiceService } from "../../services/dialog-service.service";
 
 @Component({
 	selector: "app-dialog",
@@ -7,5 +8,8 @@ import {MAT_DIALOG_DATA} from "@angular/material/dialog";
 	styleUrls: ["./dialog.component.css"],
 })
 export class DialogComponent {
-	constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
+	constructor(@Inject(MAT_DIALOG_DATA) public data: any) { }
+
+
 }
+
