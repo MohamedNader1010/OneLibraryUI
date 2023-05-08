98fca391113a3696c90eb033d879a52599d0a3ee
Mohamed Nader <m7mdnader1010@gmail.com>
2023-05-08 18:42:29 +0300

    add charts sample to dashboard

--- src/Modules/shared/components/dashboard/dashboard.component.ts
+++ src/Modules/shared/components/dashboard/dashboard.component.ts
@@ -36,7 +36,31 @@ export class DashboardComponent implements OnInit {
     responsive: false
   };
   public lineChartLegend = true;
+  
+  public pieChartOptions: ChartOptions<'pie'> = {
+    responsive: false,
+  };
+  public pieChartLabels = [ [ 'Download', 'Sales' ], [ 'In', 'Store', 'Sales' ], 'Mail Sales' ];
+  public pieChartDatasets = [ {
+    data: [ 300, 500, 100 ]
+  } ];
+  public pieChartLegend = true;
+  public pieChartPlugins = [];
 
+  public barChartLegend = true;
+  public barChartPlugins = [];
+
+  public barChartData: ChartConfiguration<'bar'>['data'] = {
+    labels: [ '2006', '2007', '2008', '2009', '2010', '2011', '2012' ],
+    datasets: [
+      { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A' },
+      { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B' }
+    ]
+  };
+
+  public barChartOptions: ChartConfiguration<'bar'>['options'] = {
+    responsive: false,
+  };
 
 
 }
