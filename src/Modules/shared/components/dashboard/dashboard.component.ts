4f472b29edef9088b5e84ecd41e37b12db784c6e
Mohamed Nader <m7mdnader1010@gmail.com>
2023-05-08 18:50:01 +0300

    add more charts samples

--- src/Modules/shared/components/dashboard/dashboard.component.ts
+++ src/Modules/shared/components/dashboard/dashboard.component.ts
@@ -11,6 +11,7 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit() {
   }
+  // line chart
   public lineChartData: ChartConfiguration<'line'>['data'] = {
     labels: [
       'January',
@@ -36,29 +37,111 @@ export class DashboardComponent implements OnInit {
     responsive: false
   };
   public lineChartLegend = true;
-  
+
+  //pie chart
   public pieChartOptions: ChartOptions<'pie'> = {
     responsive: false,
   };
-  public pieChartLabels = [ [ 'Download', 'Sales' ], [ 'In', 'Store', 'Sales' ], 'Mail Sales' ];
-  public pieChartDatasets = [ {
-    data: [ 300, 500, 100 ]
-  } ];
+  public pieChartLabels = [['Download', 'Sales'], ['In', 'Store', 'Sales'], 'Mail Sales'];
+  public pieChartDatasets = [{
+    data: [300, 500, 100]
+  }];
   public pieChartLegend = true;
   public pieChartPlugins = [];
 
+  // bar chart
   public barChartLegend = true;
   public barChartPlugins = [];
 
   public barChartData: ChartConfiguration<'bar'>['data'] = {
-    labels: [ '2006', '2007', '2008', '2009', '2010', '2011', '2012' ],
+    labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
     datasets: [
-      { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A' },
-      { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B' }
+      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
+      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
     ]
   };
 
   public barChartOptions: ChartConfiguration<'bar'>['options'] = {
+    responsive: false,
+  };
+
+  // Doughnut
+  public doughnutChartLabels: string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+  public doughnutChartDatasets: ChartConfiguration<'doughnut'>['data']['datasets'] = [
+    { data: [350, 450, 100], label: 'Series A' },
+    { data: [50, 150, 120], label: 'Series B' },
+    { data: [250, 130, 70], label: 'Series C' }
+  ];
+
+  public doughnutChartOptions: ChartConfiguration<'doughnut'>['options'] = {
+    responsive: false
+  };
+  //radar charts
+  public radarChartOptions: ChartConfiguration<'radar'>['options'] = {
+    responsive: false,
+  };
+  public radarChartLabels: string[] = ['Eating', 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling', 'Running'];
+
+  public radarChartDatasets: ChartConfiguration<'radar'>['data']['datasets'] = [
+    { data: [65, 59, 90, 81, 56, 55, 40], label: 'Series A' },
+    { data: [28, 48, 40, 19, 96, 27, 100], label: 'Series B' }
+  ];
+
+  // PolarArea
+  public polarAreaChartLabels: string[] = ['Download Sales', 'In-Store Sales', 'Mail Sales', 'Telesales', 'Corporate Sales'];
+  public polarAreaChartDatasets: ChartConfiguration<'polarArea'>['data']['datasets'] = [
+    { data: [300, 500, 100, 40, 120] }
+  ];
+  public polarAreaLegend = true;
+
+  public polarAreaOptions: ChartConfiguration<'polarArea'>['options'] = {
+    responsive: false,
+  };
+
+  // bubble chart
+  public bubbleChartOptions: ChartConfiguration<'bubble'>['options'] = {
+    responsive: false,
+    scales: {
+      x: {
+        min: 0,
+        max: 30,
+      },
+      y: {
+        min: 0,
+        max: 30,
+      }
+    }
+  };
+  public bubbleChartLegend = true;
+
+  public bubbleChartDatasets: ChartConfiguration<'bubble'>['data']['datasets'] = [
+    {
+      data: [
+        { x: 10, y: 10, r: 10 },
+        { x: 15, y: 5, r: 15 },
+        { x: 26, y: 12, r: 23 },
+        { x: 7, y: 8, r: 8 },
+      ],
+      label: 'Series A',
+    },
+  ];
+ 
+  //scatter chart
+  public scatterChartDatasets: ChartConfiguration<'scatter'>['data']['datasets'] = [
+    {
+      data: [
+        { x: 1, y: 1 },
+        { x: 2, y: 3 },
+        { x: 3, y: -2 },
+        { x: 4, y: 4 },
+        { x: 5, y: -3},
+      ],
+      label: 'Series A',
+      pointRadius: 10,
+    },
+  ];
+
+  public scatterChartOptions: ChartConfiguration<'scatter'>['options'] = {
     responsive: false,
   };
 
