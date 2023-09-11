a3176cc64fb8d94ad75e95e502ed594c4cc527ea
waleed negm <wm.negm@gmail.com>
2023-09-11 18:47:46 +0300

    //

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 let host = 'http://192.168.1.111:991/';
-// let host = 'https://localhost:7222/';
 export const environment = {
-	production: true,
-	apiUrl: `${host}api/`,
-	host: host,
+  production: true,
+  apiUrl: `${host}api/`,
+  host: host,
 };
