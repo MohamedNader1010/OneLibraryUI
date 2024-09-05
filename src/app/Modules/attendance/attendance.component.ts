1d941cc59dbbbd07e23564027ff5b6ea95dd9fb3
Mohamed Nader <m7mdnader1010@gmail.com>
2024-09-05 21:46:47 +0300

    change attendance component  columns (UI-side-filteration)

--- src/app/Modules/attendance/attendance.component.ts
+++ src/app/Modules/attendance/attendance.component.ts
@@ -31,37 +31,38 @@ export class AttendanceComponent implements OnInit {
   private initiateTableHeaders() {
     this.tableColumns = [
       {
-        columnDef: this.#translateService.instant('table.id'),
+        columnDef: "Id",
         header: this.#translateService.instant('table.id.label'),
         cell: (element: Attendance) => element.id,
       },
       {
-        columnDef: 'name',
+        // i will use first name only until handle it.
+        columnDef: 'FirstName', // this is a concatenated columns [first name and last name]
         header: 'أسم الموظف',
         cell: (element: Attendance) => element.employee,
       },
       {
-        columnDef: 'time-in',
+        columnDef: 'CheckIn',
         header: 'حضور',
         cell: (element: Attendance) => element.checkIn,
       },
       {
-        columnDef: 'time-out',
+        columnDef: 'CheckOut',
         header: 'انصراف',
         cell: (element: Attendance) => element.checkOut,
       },
       {
-        columnDef: 'formattedDuration',
+        columnDef: 'formattedDuration', // this is computed property..
         header: 'عدد الساعات',
         cell: (element: Attendance) => element.formattedDuration,
       },
       {
-        columnDef: 'formattedWorkingHours',
+        columnDef: 'formattedWorkingHours', // this is computped property..
         header: 'عدد ساعات العمل',
         cell: (element: Attendance) => element.formattedWorkingHours,
       },
       {
-        columnDef: 'formattedOverTime',
+        columnDef: 'formattedOverTime', // this is computed property..
         header: 'وقت إضافي / غير مكتمل',
         cell: (element: Attendance) => element.formattedOverTime,
       },
