6086ca0bcdfca41f37b76668db9bc8084c55c489
waleed negm <wm.negm@gmail.com>
2025-01-01 10:39:47 +0200

    update maximum error size in angular.json and change production host URL in environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
-let host = 'http://192.168.1.111:991/';
+// let host = 'http://192.168.1.111:991/';
+let host = 'http://192.168.1.111:555/';
 export const environment = {
   production: true,
   apiUrl: `${host}api/`,
